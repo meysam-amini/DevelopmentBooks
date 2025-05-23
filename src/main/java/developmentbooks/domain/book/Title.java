d45822034fe9b58eb80ebc2f59d9f137f5bb7280
meysamamini7394@gmail.com <meysamamini7394@gmail.com>
2025-05-24 01:11:04 +0200

    feat: add validations to Title. Validation tests are passing now.

--- src/main/java/developmentbooks/domain/book/Title.java
+++ src/main/java/developmentbooks/domain/book/Title.java
@@ -3,6 +3,8 @@ package developmentbooks.domain.book;
 public record Title(String value) {
 
     public Title{
-
+        if (value == null) throw new IllegalArgumentException("Title cannot be null");
+        if (value.isBlank()) throw new IllegalArgumentException("Title cannot be blank");
+        if (value.length() > 100) throw new IllegalArgumentException("Title exceeds 100 chars");
     }
 }
