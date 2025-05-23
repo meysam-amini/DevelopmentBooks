c27eb5599dc60684fb0f3bf6ad8e4b596a44c373
meysamamini7394@gmail.com <meysamamini7394@gmail.com>
2025-05-24 01:18:43 +0200

    feat: add validations to PublicationYear. validation tests pass now.

--- src/main/java/developmentbooks/domain/book/PublicationYear.java
+++ src/main/java/developmentbooks/domain/book/PublicationYear.java
@@ -1,8 +1,11 @@
 package developmentbooks.domain.book;
 
+import java.time.Year;
+
 public record PublicationYear(int value) {
 
-    public PublicationYear{
-
+    public PublicationYear {
+        if (value < 1800) throw new IllegalArgumentException("Year cannot be before 1800");
+        if (value > Year.now().getValue()) throw new IllegalArgumentException("Year cannot be in future");
     }
 }
