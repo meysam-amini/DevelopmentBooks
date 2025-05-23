cd76fb9980db52e6c9aa6e4e8383121832c25d7e
meysamamini7394@gmail.com <meysamamini7394@gmail.com>
2025-05-23 23:50:28 +0200

    chore: initial commit

---
+++ src/main/java/developmentbooks/DevelopmentBooksApplication.java
@@ -0,0 +1,13 @@
+package developmentbooks;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopmentBooksApplication.class, args);
+    }
+}
