529bb64741103ec31586328d335621df9bc7efa0
meysamamini7394@gmail.com <meysamamini7394@gmail.com>
2025-05-24 01:14:59 +0200

    feat: validations added to Author obj. tests pass.

--- src/main/java/developmentbooks/domain/book/Author.java
+++ src/main/java/developmentbooks/domain/book/Author.java
@@ -3,6 +3,7 @@ package developmentbooks.domain.book;
 public record Author(String value) {
 
     public Author{
-
+        if (value == null) throw new IllegalArgumentException("Author cannot be null");
+        if (value.isBlank()) throw new IllegalArgumentException("Author cannot be blank");
     }
 }
