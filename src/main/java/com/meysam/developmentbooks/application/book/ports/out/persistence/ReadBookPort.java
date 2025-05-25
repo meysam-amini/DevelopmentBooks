7cabc6b16bb278f7d1ecc2902b379dcfef47fca5
meysamamini7394@gmail.com <meysamamini7394@gmail.com>
2025-05-25 15:39:37 +0200

    fix: update ReadBookPort.

--- src/main/java/com/meysam/developmentbooks/application/book/ports/out/persistence/ReadBookPort.java
+++ src/main/java/com/meysam/developmentbooks/application/book/ports/out/persistence/ReadBookPort.java
@@ -1,12 +1,13 @@
 package com.meysam.developmentbooks.application.book.ports.out.persistence;
 
+import com.meysam.developmentbooks.application.book.ports.out.persistence.query.BookSearchCriteria;
 import com.meysam.developmentbooks.domain.book.Book;
 
 import java.util.List;
 
 public interface ReadBookPort {
 
-    List<Book> findAll();
+    List<Book> findAll(BookSearchCriteria searchCriteria);
 
-    Boolean existsByIsbn(String isbn);
+    boolean existsByIsbn(String isbn);
 }
